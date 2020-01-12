b47cebdbf5c1c6cd656a9753f68d90a414b68a41
Alejandro Hernández <Alejandrehl@icloud.com>
2020-01-12 14:14:34 -0300

    Refactor utilizando @ModelAttribute

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -35,14 +36,17 @@ public class IndexController {
 
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de usuarios");
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios() {
 		List<Usuario> usuarios = Arrays.asList(new Usuario("Alejandro", "Hernandez", "Alejandrehl@icloud.com"),
 				new Usuario("Fernanda", "Coloma", "Fer.ignacia19@gmail.com"),
 				new Usuario("Julio", "Hernández", "juldezcist@gmail.com"));
 
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-
-		return "listar";
+		return usuarios;
 	}
 
 }
